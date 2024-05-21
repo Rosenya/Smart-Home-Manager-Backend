ea15018ecd3fb5f7929b4c0e21e0dcfe8555ae9d
Rosenya <lajluch@gmail.com>
2024-05-21 20:07:21 +0200

    Initial commit

---
+++ src/main/java/com/ArkadiaPlocienniczak/SmartHomeService/SmartHomeServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.ArkadiaPlocienniczak.SmartHomeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeServiceAppApplication.class, args);
+	}
+
+}
